e6a3b35440a843599c6701a14a19e2a2fabf18d5
rhithesh <hithesh@Hitheshs-MacBook-Air.local>
2024-09-24 01:37:29 +0530

    change

--- prisma/migrations/20240923200659_new/migration.sql
+++ prisma/migrations/20240923200659_new/migration.sql
@@ -17,6 +17,15 @@ CREATE TABLE "Session" (
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
